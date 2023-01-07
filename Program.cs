87cd22eabfe989dcc1771dfb43f652eb27567e42
Emrul <emrul.hoque@gmail.com>
2023-01-07 20:05:03 +0600

    update

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Array;
+
+class Program
+
+{
+    static void Main()
+
+    {
+        int[] arr = new int[3];
+        string[] order = {"first", "second", "third"};
+        int i;
+
+        for (i = 0; i < 3; i++)
+        {
+            Console.Write("Enter your "+ order[i]+ " number ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        for (i = 0; i < 3; i++)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Your "+ order[i]  +" number is " + arr[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("Press ENTER key");
+        Console.Read();
+    }
+
+}
